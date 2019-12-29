1e7350945f9811fd006a112cc5012063b676ec1f
Amol <amolborkar5@gmail.com>
2019-12-30 00:34:23 +0530

    Added support for bottom navigation.

--- app/src/main/java/com/halalrishtey/MainActivity.kt
+++ app/src/main/java/com/halalrishtey/MainActivity.kt
@@ -2,11 +2,17 @@ package com.halalrishtey
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        bottom_navigation.setupWithNavController(findNavController(R.id.nav_host_fragment))
     }
 }
