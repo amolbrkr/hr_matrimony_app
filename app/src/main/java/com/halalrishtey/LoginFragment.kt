6c2e17aad7552d2ed243044ff201dbd5ea79aaea
Amol <amolborkar5@gmail.com>
2019-12-31 01:12:31 +0530

    Created a login fragment.

---
+++ app/src/main/java/com/halalrishtey/LoginFragment.kt
@@ -0,0 +1,29 @@
+package com.halalrishtey
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_login.*
+
+
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        registerButton.setOnClickListener { _ ->
+            findNavController().navigate(R.id.registerFragment)
+        }
+    }
+
+}
