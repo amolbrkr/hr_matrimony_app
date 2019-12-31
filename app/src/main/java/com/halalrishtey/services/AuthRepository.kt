647a9ab504552500a63fa10b565d0349c585f0ed
Amol <amolborkar5@gmail.com>
2020-01-01 01:33:09 +0530

    Renamed and updated to work with AuthData

--- app/src/main/java/com/halalrishtey/services/AuthRepository.kt
+++ app/src/main/java/com/halalrishtey/services/AuthRepository.kt
@@ -5,38 +5,27 @@ import androidx.lifecycle.MutableLiveData
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
+import com.halalrishtey.models.AuthData
 
-class AuthService {
+class AuthRepository {
     private val firebaseAuth: FirebaseAuth by lazy {
         FirebaseAuth.getInstance()
     }
 
-    fun getCurrentUser(): MutableLiveData<FirebaseUser> {
+    fun getCurrentUser() = firebaseAuth.currentUser
 
-        val user = MutableLiveData<FirebaseUser>()
-
-        if (firebaseAuth.currentUser != null) {
-            user.value = firebaseAuth.currentUser
-        } else {
-            user.value = null
-        }
-
-        return user
-    }
-
-    fun createNewUser(email: String, password: String): MutableLiveData<FirebaseUser> {
-        val authenticatedUserLiveData = MutableLiveData<FirebaseUser>()
+    fun createNewUser(email: String, password: String): MutableLiveData<AuthData> {
+        val authenticatedUserLiveData = MutableLiveData<AuthData>()
 
         firebaseAuth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener { task: Task<AuthResult> ->
                 if (task.isSuccessful) {
-                    Log.d("AuthService", "Created new user! $email")
-                    authenticatedUserLiveData.value = task.result?.user
+                    Log.d("AuthService", "Created new user! ${task.result?.user?.email}")
+                    authenticatedUserLiveData.postValue(AuthData(task.result?.user, null))
 
                     task.result?.user?.let {
-                        DatabaseService.getDbInstance().collection("users")
-                            .document(it.uid).set(it).addOnCompleteListener { task: Task<Void> ->
+                        DatabaseRepository.getDbInstance().collection("users")
+                            .document(it.uid).set(it).addOnCompleteListener {
                                 Log.d("AuthService", "Added new user to database.")
                             }
                     }
@@ -45,28 +34,30 @@ class AuthService {
                         "AuthService",
                         "Failed to create new user! $email ${task.exception?.message}"
                     )
-                    authenticatedUserLiveData.value = null
+                    authenticatedUserLiveData.postValue(
+                        AuthData(null, task.exception?.message)
+                    )
                 }
             }
-
 
         return authenticatedUserLiveData
     }
 
-    fun loginWithEmail(email: String, password: String): MutableLiveData<FirebaseUser> {
-        val userLiveData = MutableLiveData<FirebaseUser>()
+    fun loginWithEmail(email: String, password: String): MutableLiveData<AuthData> {
+        val userLiveData = MutableLiveData<AuthData>()
 
         firebaseAuth.signInWithEmailAndPassword(email, password)
             .addOnCompleteListener { task: Task<AuthResult> ->
                 if (task.isSuccessful) {
                     Log.d("AuthService", "Sign in with new user! $email")
-                    userLiveData.value = task.result?.user
+                    userLiveData.value =
+                        AuthData(task.result?.user, null)
                 } else {
                     Log.d(
                         "AuthService",
                         "Failed to sign in! $email ${task.exception?.message}"
                     )
-                    userLiveData.value = null
+                    userLiveData.value = AuthData(null, task.exception?.message)
                 }
             }
 
