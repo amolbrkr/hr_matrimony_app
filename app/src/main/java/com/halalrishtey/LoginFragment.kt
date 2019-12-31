6fba44bd2e9bf23256d56fb86159cd40cd2f6f15
Amol <amolborkar5@gmail.com>
2020-01-01 01:31:57 +0530

    Initial implementation of auth functionality.

--- app/src/main/java/com/halalrishtey/LoginFragment.kt
+++ app/src/main/java/com/halalrishtey/LoginFragment.kt
@@ -5,12 +5,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
+import com.halalrishtey.viewmodels.UserAuthViewModel
 import kotlinx.android.synthetic.main.fragment_login.*
 
 
 class LoginFragment : Fragment() {
+
+    private val userAuthVM: UserAuthViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,6 +27,25 @@ class LoginFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        login_button.setOnClickListener {
+            val email = email_editText.editText?.text
+            val pw = password_editText.editText?.text
+
+            userAuthVM.signIn(email.toString(), pw.toString())
+                .observe(viewLifecycleOwner, Observer {
+                    if (it?.errorMessage != null) {
+                        Snackbar.make(view, "Error: ${it.errorMessage}", Snackbar.LENGTH_SHORT)
+                            .show()
+                    }
+
+                    if (it?.data != null) {
+                        Toast.makeText(context, "Logged in: ${it.data.email}", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                })
+        }
+
         goToRegisterButton.setOnClickListener { _ ->
             findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
         }
--- app/src/main/java/com/halalrishtey/RegisterFragment.kt
+++ app/src/main/java/com/halalrishtey/RegisterFragment.kt
@@ -4,10 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
+import com.google.android.material.snackbar.Snackbar
+import com.halalrishtey.models.AuthData
+import com.halalrishtey.viewmodels.UserAuthViewModel
+import kotlinx.android.synthetic.main.fragment_register.*
 
 
 class RegisterFragment : Fragment() {
+    private val userAuthVM: UserAuthViewModel by activityViewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -16,5 +25,30 @@ class RegisterFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_register, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
+        register_button.setOnClickListener {
+            val email = registerEmail_editText.editText?.text
+            val pw = registerPassword_editText.editText?.text
+
+            userAuthVM.signUp(email.toString(), pw.toString())
+                .observe(viewLifecycleOwner, Observer { authData: AuthData ->
+
+                    if (authData.errorMessage != null) {
+                        Snackbar.make(
+                            view,
+                            "Error: ${authData.errorMessage}",
+                            Snackbar.LENGTH_SHORT
+                        )
+                            .show()
+                    }
+
+                    if (authData.data != null) {
+                        Toast.makeText(context, "User: ${authData.data.email}", Toast.LENGTH_SHORT)
+                            .show()
+
+                    }
+                })
+        }
+    }
 }
