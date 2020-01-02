798e64faf7bfe6937bcd2fa67723e47650e3f98c
Amol <amolborkar5@gmail.com>
2020-01-03 00:08:35 +0530

    Layout completed and added functionality

--- app/src/main/java/com/halalrishtey/PersonalDetailsFragment.kt
+++ app/src/main/java/com/halalrishtey/PersonalDetailsFragment.kt
@@ -5,7 +5,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
+import com.halalrishtey.models.User
+import kotlinx.android.synthetic.main.fragment_personal_details.*
 
 class PersonalDetailsFragment : Fragment() {
 
@@ -15,5 +18,22 @@ class PersonalDetailsFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_personal_details, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val userData = arguments?.get("userInfo") as User
+
+        val CreatedForStrings =
+            arrayOf("Myself", "Relative", "Son", "Daughter", "Brother", "Sister")
+
+
+        val createdForAdapter: ArrayAdapter<String> =
+            ArrayAdapter<String>(
+                requireContext(),
+                R.layout.dropdown_menu_popup_item,
+                CreatedForStrings
+            )
+
+        createdFor_dropDown.setAdapter(createdForAdapter)
     }
 }
