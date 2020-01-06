14932171fcaf93fd871e16b4991576409c4a7458
Amol <amolborkar5@gmail.com>
2020-01-07 00:09:55 +0530

    Added datepicker for dob

--- app/src/main/java/com/halalrishtey/PersonalDetailsFragment.kt
+++ app/src/main/java/com/halalrishtey/PersonalDetailsFragment.kt
@@ -1,39 +1,97 @@
 package com.halalrishtey
 
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
-import com.halalrishtey.models.User
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import com.halalrishtey.models.CreatedFor
+import com.halalrishtey.models.Gender
+import com.halalrishtey.viewmodels.UserAuthViewModel
 import kotlinx.android.synthetic.main.fragment_personal_details.*
+import java.util.*
+
 
 class PersonalDetailsFragment : Fragment() {
+    private val userAuthVM: UserAuthViewModel by activityViewModels()
+    private var savedViewInstance: View? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_personal_details, container, false)
+        return if (savedViewInstance != null) {
+            savedViewInstance
+        } else {
+            savedViewInstance =
+                inflater.inflate(R.layout.fragment_personal_details, container, false)
+            savedViewInstance
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        val userData = arguments?.get("userInfo") as User
-
-        val CreatedForStrings =
+        val createdForStrings =
             arrayOf("Myself", "Relative", "Son", "Daughter", "Brother", "Sister")
-
 
         val createdForAdapter: ArrayAdapter<String> =
             ArrayAdapter<String>(
                 requireContext(),
                 R.layout.dropdown_menu_popup_item,
-                CreatedForStrings
+                createdForStrings
             )
 
         createdFor_dropDown.setAdapter(createdForAdapter)
+        val cal: Calendar = Calendar.getInstance()
+
+
+        val datePickerDialog =
+            DatePickerDialog.OnDateSetListener { v, year, monthOfYear, dayOfMonth ->
+                cal.set(Calendar.YEAR, year)
+                cal.set(Calendar.MONTH, monthOfYear)
+                cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+
+                val date = "$dayOfMonth / ${monthOfYear + 1} / $year"
+                userAuthVM.newUser.value?.dateOfBirth =
+                    GregorianCalendar(year, monthOfYear + 1, dayOfMonth).time
+                dob_button.text = date
+            }
+
+        dob_button.setOnClickListener {
+            DatePickerDialog(
+                requireContext(), datePickerDialog,
+                cal.get(Calendar.YEAR), cal.get(Calendar.MONTH),
+                cal.get(Calendar.DAY_OF_MONTH)
+            ).show()
+        }
+
+        pdNextButton.setOnClickListener {
+            userAuthVM.newUser.value?.displayName =
+                name_editText.editText?.text.toString()
+
+            userAuthVM.newUser.value?.phoneNumber =
+                phone_editText.editText?.text.toString().toLong()
+
+            userAuthVM.newUser.value?.createdFor =
+                CreatedFor.valueOf(createdFor_dropDown.text.toString())
+
+            userAuthVM.newUser.value?.pinCode =
+                Integer.parseInt(pincode_editText.editText?.text.toString())
+
+            userAuthVM.newUser.value?.gender = Gender.valueOf(
+                if (maleSelector.isChecked) {
+                    "Male"
+                } else {
+                    "Female"
+                }
+            )
+            findNavController().navigate(
+                R.id.action_personalDetailsFragment_to_professionalDetails
+            )
+        }
     }
 }
