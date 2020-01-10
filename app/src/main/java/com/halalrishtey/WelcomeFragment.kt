0c4a997d18c3fce51b021b83469aa1b381f9109a
Amol <amolborkar5@gmail.com>
2020-01-11 01:15:30 +0530

    Added permissions for reading external storage and camera

--- app/src/main/java/com/halalrishtey/WelcomeFragment.kt
+++ app/src/main/java/com/halalrishtey/WelcomeFragment.kt
@@ -18,8 +18,8 @@ import kotlinx.android.synthetic.main.fragment_welcome.*
 class WelcomeFragment : Fragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_welcome, container, false)
     }
@@ -30,16 +30,18 @@ class WelcomeFragment : Fragment() {
         getStartedButton.setOnClickListener {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 if (ContextCompat.checkSelfPermission(
-                        requireContext(),
-                        Manifest.permission.ACCESS_FINE_LOCATION
-                    ) != PackageManager.PERMISSION_GRANTED
+                                requireContext(),
+                                Manifest.permission.ACCESS_FINE_LOCATION
+                        ) != PackageManager.PERMISSION_GRANTED
                 ) {
                     ActivityCompat.requestPermissions(
-                        requireActivity(),
-                        arrayOf(
-                            Manifest.permission.ACCESS_FINE_LOCATION
-                        ),
-                        1
+                            requireActivity(),
+                            arrayOf(
+                                    Manifest.permission.ACCESS_FINE_LOCATION,
+                                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                                    Manifest.permission.CAMERA
+                            ),
+                            1
                     )
                 }
             }
@@ -51,10 +53,10 @@ class WelcomeFragment : Fragment() {
 
 private const val ARG_RES_ID = "image_res_id"
 private val imageResources =
-    listOf(R.mipmap.welcome_0, R.mipmap.welcome_1, R.mipmap.welcome_2, R.mipmap.welcome_3)
+        listOf(R.mipmap.welcome_0, R.mipmap.welcome_1, R.mipmap.welcome_2, R.mipmap.welcome_3)
 
 class WelcomeViewPagerAdapter(fragment: Fragment) :
-    FragmentStateAdapter(fragment) {
+        FragmentStateAdapter(fragment) {
     override fun getItemCount(): Int = 4
 
     override fun createFragment(position: Int): Fragment {
