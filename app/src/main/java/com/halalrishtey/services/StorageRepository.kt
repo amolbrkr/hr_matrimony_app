1a858d86533dbdf3b72a3f39bb6208ab7593848d
Amol <amolborkar5@gmail.com>
2020-01-11 01:11:38 +0530

    Created sperate repository for storage related functions

---
+++ app/src/main/java/com/halalrishtey/services/StorageRepository.kt
@@ -0,0 +1,42 @@
+package com.halalrishtey.services
+
+import android.net.Uri
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import com.google.firebase.storage.FirebaseStorage
+
+data class UploadResult(
+        var fileUrl: String?,
+        var errorMsg: String?
+)
+
+object StorageRepository {
+    private val storageReference by lazy {
+        FirebaseStorage.getInstance()
+    }
+
+    val imagesReference = storageReference.getReference("Images")
+
+    fun uploadImgToStorage(imageUri: Uri): MutableLiveData<UploadResult> {
+        val fileLink = MutableLiveData<UploadResult>();
+
+        val ref = imagesReference.child("${System.currentTimeMillis()}")
+
+        ref.putFile(imageUri)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        ref.downloadUrl.addOnSuccessListener {
+                            fileLink.value = UploadResult(it.toString(), null)
+                        }
+                    } else {
+                        fileLink.value = UploadResult(null, "File upload failed!")
+                    }
+                }
+        return fileLink
+    }
+
+    fun getFileExt(filePath: String): String? {
+        Log.d("StorageRepository", filePath)
+        return filePath.substring(filePath.lastIndexOf("."))
+    }
+}
