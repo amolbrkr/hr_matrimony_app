557a004710e89b34911363adc5ec2b01860c5cc4
Amol <amolborkar5@gmail.com>
2020-01-11 01:12:35 +0530

    Created new services for live location updates

---
+++ app/src/main/java/com/halalrishtey/viewmodels/LocationLiveData.kt
@@ -0,0 +1,60 @@
+package com.halalrishtey.viewmodels
+
+import android.content.Context
+import android.location.Location
+import android.util.Log
+import androidx.lifecycle.LiveData
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+
+
+class LocationLiveData(context: Context) : LiveData<Location>() {
+    private val fusedLocationProviderClient =
+            LocationServices.getFusedLocationProviderClient(context)
+
+    private val locationCallback = object : LocationCallback() {
+        override fun onLocationResult(p0: LocationResult?) {
+            super.onLocationResult(p0)
+            p0 ?: return
+            for (loc in p0.locations) {
+                value = loc
+            }
+        }
+    }
+
+    companion object {
+        val locationRequest: LocationRequest = LocationRequest.create().apply {
+            interval = 60000
+            fastestInterval = 10000
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+    }
+
+    override fun onActive() {
+        super.onActive()
+
+        fusedLocationProviderClient.lastLocation.addOnSuccessListener { location: Location? ->
+            location?.also {
+                value = location
+            }
+        }.addOnFailureListener { e: Exception ->
+            Log.d("LocationLiveData", e.message ?: "FusedProviderClient Failed!")
+            value = null
+        }
+
+        fusedLocationProviderClient.requestLocationUpdates(
+                locationRequest,
+                locationCallback,
+                null
+        )
+    }
+
+    override fun onInactive() {
+        super.onInactive()
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+    }
+
+
+}
---
+++ app/src/main/java/com/halalrishtey/viewmodels/SharedViewModel.kt
@@ -0,0 +1,9 @@
+package com.halalrishtey.viewmodels
+
+import android.os.Bundle
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SharedViewModel : ViewModel() {
+    val bundleFromUploadImageFragment = MutableLiveData<Bundle>()
+}
