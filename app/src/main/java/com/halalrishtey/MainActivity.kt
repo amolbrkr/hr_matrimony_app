9c2371813de8570cdc358e0669e53cdd962a87aa
Amol <amolborkar5@gmail.com>
2020-01-11 01:12:56 +0530

    Removed a comment

--- app/src/main/java/com/halalrishtey/MainActivity.kt
+++ app/src/main/java/com/halalrishtey/MainActivity.kt
@@ -27,7 +27,6 @@ class MainActivity : AppCompatActivity() {
             startActivity(i)
             this.finish()
         } else {
-            //TODO: Load user data using this uid.
             Toast.makeText(applicationContext, uid.toString(), Toast.LENGTH_SHORT).show()
         }
     }
