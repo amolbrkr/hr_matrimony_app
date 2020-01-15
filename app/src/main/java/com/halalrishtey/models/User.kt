fc7b9015487f5012dd710d17b554fda6e1db9c66
Amol <amolborkar5@gmail.com>
2020-01-15 15:48:41 +0530

    Reformated code.

--- app/src/main/java/com/halalrishtey/models/User.kt
+++ app/src/main/java/com/halalrishtey/models/User.kt
@@ -12,33 +12,33 @@ enum class Gender {
 
 @Parcelize
 data class User(
-        val email: String,      //done //Must be first
-        var uid: String? = "",      //done
-        var displayName: String = "", //done
-        var age: Int? = null,          //done
-        var photoUrl: String = "",      //done
-        var idProofUrl: String = "",        //done
-        var phoneNumber: Number = 0,        //done
-        var gender: Gender = Gender.Male, //done
-        var createdFor: String = "",
-        var createdAt: Long = System.currentTimeMillis(),
-        var lastSignInAt: Long = System.currentTimeMillis(),
-        var isPhoneVerified: Boolean = false,
-        var address: String = "",       //done
-        var dateOfBirth: Date? = null,      //done
-        var height: String = "",            //done
-        var education: String = "",
-        var occupation: String = "",        //done
-        var workExperience: String = "",        //done
-        var annualIncome: Number = 0,       //done
-        var organizationName: String = "",      //done
-        var sect: String = "",      //done
-        var dargah: String = "",        //done
-        var maritalStatus: String = "",      //done
-        var locationLat: Double = 0.0,      //done
-        var locationLong: Double = 0.0,     //done
-        var countryCode: String = "IN",     //done
-        var pincode: String = ""        //done
+    val email: String,      //done //Must be first
+    var uid: String? = "",      //done
+    var displayName: String = "", //done
+    var age: Int? = null,          //done
+    var photoUrl: String = "",      //done
+    var idProofUrl: String = "",        //done
+    var phoneNumber: Number = 0,        //done
+    var gender: Gender = Gender.Male, //done
+    var createdFor: String = "",
+    var createdAt: Long = System.currentTimeMillis(),
+    var lastSignInAt: Long = System.currentTimeMillis(),
+    var isPhoneVerified: Boolean = false,
+    var address: String = "",       //done
+    var dateOfBirth: Date? = null,      //done
+    var height: String = "",            //done
+    var education: String = "",
+    var occupation: String = "",        //done
+    var workLocation: String = "",        //done
+    var annualIncome: Number = 0,       //done
+    var organizationName: String = "",      //done
+    var sect: String = "",      //done
+    var dargah: String = "",        //done
+    var maritalStatus: String = "",      //done
+    var locationLat: Double = 0.0,      //done
+    var locationLong: Double = 0.0,     //done
+    var countryCode: String = "IN",     //done
+    var pincode: String = ""        //done
 ) : Parcelable {
     constructor(firebaseUser: FirebaseUser) :
             this(uid = firebaseUser.uid, email = firebaseUser.email!!)
