77ee2cac46c8b19b0b159c6d3f0e341f4244b152
Amminexx <amolborkar5@gmail.com>
2020-02-22 17:56:29 +0800

    Shifted code for converting DocumentSnapshot to User object to a function

--- app/src/main/java/com/halalrishtey/CustomUtils.kt
+++ app/src/main/java/com/halalrishtey/CustomUtils.kt
@@ -4,9 +4,43 @@ import android.content.Context
 import android.location.Address
 import android.location.Geocoder
 import android.util.Log
+import com.google.firebase.firestore.DocumentSnapshot
+import com.halalrishtey.models.Gender
+import com.halalrishtey.models.User
 import java.util.*
 
 object CustomUtils {
+
+    fun convertToUser(snapshot: DocumentSnapshot): User {
+        return User(
+            email = snapshot.get("email").toString(),
+            uid = snapshot.get("uid").toString(),
+            displayName = snapshot.get("displayName").toString(),
+            age = snapshot.get("age").toString().toInt(),
+            photoUrl = snapshot.get("photoUrl").toString(),
+            idProofUrl = snapshot.get("idProofUrl").toString(),
+            phoneNumber = snapshot.get("phoneNumber") as Number,
+            gender = Gender.valueOf(snapshot.get("gender").toString()),
+            createdFor = snapshot.get("createdFor").toString(),
+            lastSignInAt = System.currentTimeMillis(),
+            address = snapshot.get("address")?.toString() ?: "Not found",
+            height = snapshot.get("height").toString(),
+            education = snapshot.get("education").toString(),
+            workLocation = snapshot.get("workLocation").toString(),
+            sect = snapshot.get("sect").toString(),
+            dargah = snapshot.get("dargah").toString(),
+            maritalStatus = snapshot.get("maritalStatus").toString(),
+            locationLat = snapshot.get("locationLat")?.toString()?.toDouble() ?: 0.0,
+            locationLong = snapshot.get("locationLong")?.toString()?.toDouble()
+                ?: 0.0,
+            countryCode = snapshot.get("countryCode").toString(),
+            pincode = snapshot.get("pincode")?.toString() ?: "Not found",
+            isOTPVerified = snapshot.get("otpverified") as Boolean,
+            countryCallingCode = snapshot.get("countryCallingCode").toString()
+            //interestCount = it.get("interestCount") as Int
+        )
+    }
+
     fun convertCoordsToAddr(context: Context, latitude: Double, longitude: Double): Address? {
         val geocoder = Geocoder(context, Locale.getDefault())
 
--- app/src/main/java/com/halalrishtey/services/UserRepository.kt
+++ app/src/main/java/com/halalrishtey/services/UserRepository.kt
@@ -3,6 +3,7 @@ package com.halalrishtey.services
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import com.google.firebase.firestore.FieldValue
+import com.halalrishtey.CustomUtils
 import com.halalrishtey.models.Gender
 import com.halalrishtey.models.User
 
@@ -39,34 +40,7 @@ object UserRepository {
             .get()
             .addOnCompleteListener { task ->
                 if (task.isSuccessful) {
-                    fetchedUser.value = User(
-                        email = task.result?.data?.get("email").toString(),
-                        uid = task.result?.data?.get("uid").toString(),
-                        displayName = task.result?.data?.get("displayName").toString(),
-                        age = task.result?.data?.get("age").toString().toInt(),
-                        photoUrl = task.result?.data?.get("photoUrl").toString(),
-                        idProofUrl = task.result?.data?.get("idProofUrl").toString(),
-                        phoneNumber = task.result?.data?.get("phoneNumber") as Number,
-                        gender = Gender.valueOf(task.result?.data?.get("gender").toString()),
-                        createdFor = task.result?.data?.get("createdFor").toString(),
-                        lastSignInAt = System.currentTimeMillis(),
-                        address = task.result?.data?.get("address")?.toString() ?: "Not found",
-                        height = task.result?.data?.get("height").toString(),
-                        education = task.result?.data?.get("education").toString(),
-                        workLocation = task.result?.data?.get("workLocation").toString(),
-                        sect = task.result?.data?.get("sect").toString(),
-                        dargah = task.result?.data?.get("dargah").toString(),
-                        maritalStatus = task.result?.data?.get("maritalStatus").toString(),
-                        locationLat = task.result?.data?.get("locationLat")?.toString()?.toDouble()
-                            ?: 0.0,
-                        locationLong = task.result?.data?.get("locationLong")?.toString()?.toDouble()
-                            ?: 0.0,
-                        countryCode = task.result?.data?.get("countryCode").toString(),
-                        pincode = task.result?.data?.get("pincode")?.toString() ?: "Not found",
-                        isOTPVerified = task.result?.data?.get("otpverified") as Boolean,
-                        countryCallingCode = task.result?.data?.get("countryCallingCode").toString()
-                        //interestCount = it.get("interestCount") as Int
-                    )
+                    fetchedUser.value = CustomUtils.convertToUser(task.result!!)
                 } else {
                     fetchedUser.value = null
                     Log.d("UserRepository", "")
@@ -87,35 +61,7 @@ object UserRepository {
 
                     task.result?.documents?.forEach {
                         //Skipped date of birth below
-                        tempList.add(
-                            User(
-                                email = it.get("email").toString(),
-                                uid = it.get("uid").toString(),
-                                displayName = it.get("displayName").toString(),
-                                age = it.get("age").toString().toInt(),
-                                photoUrl = it.get("photoUrl").toString(),
-                                idProofUrl = it.get("idProofUrl").toString(),
-                                phoneNumber = it.get("phoneNumber") as Number,
-                                gender = Gender.valueOf(it.get("gender").toString()),
-                                createdFor = it.get("createdFor").toString(),
-                                lastSignInAt = System.currentTimeMillis(),
-                                address = it.get("address")?.toString() ?: "Not found",
-                                height = it.get("height").toString(),
-                                education = it.get("education").toString(),
-                                workLocation = it.get("workLocation").toString(),
-                                sect = it.get("sect").toString(),
-                                dargah = it.get("dargah").toString(),
-                                maritalStatus = it.get("maritalStatus").toString(),
-                                locationLat = it.get("locationLat")?.toString()?.toDouble() ?: 0.0,
-                                locationLong = it.get("locationLong")?.toString()?.toDouble()
-                                    ?: 0.0,
-                                countryCode = it.get("countryCode").toString(),
-                                pincode = it.get("pincode")?.toString() ?: "Not found",
-                                isOTPVerified = it.get("otpverified") as Boolean,
-                                countryCallingCode = it.get("countryCallingCode").toString()
-                                //interestCount = it.get("interestCount") as Int
-                            )
-                        )
+                        tempList.add(CustomUtils.convertToUser(it))
                     }
                     listOfUsers.value = tempList
                 } else {
