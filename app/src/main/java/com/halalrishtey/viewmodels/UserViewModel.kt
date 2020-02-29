fcb097cf844107e44385e9fe6969ddabedfc8120
Amol <amolborkar5@gmail.com>
2020-03-01 00:25:11 +0530

    Improved getUser function

--- app/src/main/java/com/halalrishtey/viewmodels/UserViewModel.kt
+++ app/src/main/java/com/halalrishtey/viewmodels/UserViewModel.kt
@@ -1,14 +1,27 @@
 package com.halalrishtey.viewmodels
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.google.android.gms.common.UserRecoverableException
 import com.halalrishtey.models.User
 import com.halalrishtey.services.UserRepository
 
 class UserViewModel : ViewModel() {
     val currentUserId = MutableLiveData<String>()
-    fun getUser(uid: String) = UserRepository.getProfileOfUser(uid)
+    var currentUserProfile = MutableLiveData<User>()
+
+    fun getUser(uid: String): MutableLiveData<User> {
+        return if (uid == currentUserId.value) {
+            if (currentUserProfile.value == null) {
+                Log.d("UserViewModel", "Made repo call to get user!")
+                currentUserProfile = UserRepository.getProfileOfUser(uid)
+                currentUserProfile
+            } else currentUserProfile
+        } else UserRepository.getProfileOfUser(uid)
+    }
+
+    fun getProfilesByIds(listOfIds: ArrayList<String>) = UserRepository.getProfilesByIds(listOfIds)
+
     fun shortlistUser(currentUserId: String, targetUserId: String) =
         UserRepository.shortlistUser(currentUserId, targetUserId)
 }
