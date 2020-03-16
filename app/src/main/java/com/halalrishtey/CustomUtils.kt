9d63a588850b73a2d51b4c4d7ecd3060d9cb293a
Amol <amolborkar5@gmail.com>
2020-03-16 23:28:44 +0530

    Notification function was added

--- app/src/main/java/com/halalrishtey/CustomUtils.kt
+++ app/src/main/java/com/halalrishtey/CustomUtils.kt
@@ -1,9 +1,16 @@
 package com.halalrishtey
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.location.Address
 import android.location.Geocoder
+import android.os.Build
 import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.google.firebase.firestore.DocumentSnapshot
 import com.halalrishtey.models.Gender
 import com.halalrishtey.models.User
@@ -11,6 +18,49 @@ import java.util.*
 
 
 object CustomUtils {
+    fun displayLocalNotif(
+        context: Context,
+        notifId: Int,
+        tapOpenActivityClass: Class<*>,
+        channelName: String,
+        notifTitle: String,
+        notifText: String
+    ) {
+        val notificationManager =
+            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val channelId = channelName.toLowerCase(Locale.ROOT).replace(" ", "_")
+
+        val intent = Intent(context, tapOpenActivityClass).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(context, 0, intent, 0)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId,
+                channelName,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            channel.enableLights(true)
+            channel.enableVibration(true)
+            channel.setShowBadge(true)
+
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        val builder = NotificationCompat.Builder(context, channelId)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle(notifTitle)
+            .setContentText(notifText)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+
+        with(NotificationManagerCompat.from(context)) {
+            notify(notifId, builder.build())
+        }
+    }
 
     fun convertToUser(doc: DocumentSnapshot): User {
         return User(
