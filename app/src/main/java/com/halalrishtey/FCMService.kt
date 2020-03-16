aaeaa06bc484b3397f982f6ba453894a3c0f4b39
Amol <amolborkar5@gmail.com>
2020-03-16 23:18:46 +0530

    Added code for push notifications.

---
+++ app/src/main/java/com/halalrishtey/FCMService.kt
@@ -0,0 +1,30 @@
+package com.halalrishtey
+
+
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class FCMService : FirebaseMessagingService() {
+
+    override fun onNewToken(p0: String) {
+        super.onNewToken(p0)
+
+        Log.d("FCMService", "New token: " + p0)
+    }
+
+    override fun onMessageReceived(p0: RemoteMessage) {
+        super.onMessageReceived(p0)
+
+        if (p0.notification != null) {
+            CustomUtils.displayLocalNotif(
+                this,
+                21,
+                MainActivity::class.java,
+                "push_notif",
+                "You've recieved a message!",
+                p0.notification?.body.toString()
+            );
+        }
+    }
+}
