ceaa548ca59c3c0c364e37b15a7cc90a0a0bebaa
Amol <amolborkar5@gmail.com>
2020-03-23 00:13:20 +0530

    Implemented chat window

--- app/src/main/java/com/halalrishtey/ChatActivity.kt
+++ app/src/main/java/com/halalrishtey/ChatActivity.kt
@@ -1,12 +1,60 @@
 package com.halalrishtey
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import com.halalrishtey.viewmodels.UserViewModel
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_chat.*
 
 class ChatActivity : AppCompatActivity() {
+
+    private val userVM: UserViewModel by viewModels()
+    private var conversationId: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
+        setSupportActionBar(chatToolbar)
+
+        conversationId = intent.extras?.getString("conversationId")
+        val senderId = intent.extras?.getString("senderId")
+        val title = intent.extras?.getString("targetName")
+        val targetPhotoUrl = intent.extras?.getString("targetPhotoUrl")
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        supportActionBar?.title = ""
+
+        chatTitleTextView.text = title
+
+        sendChatBtn.setOnClickListener {
+            var msg = chatTextInp.editText?.text.toString()
+            msg = msg.trim()
+
+            if (msg.length > 1 && conversationId != null && senderId != null) {
+                Log.d("ChatActivity", "Message sent: $msg")
+                userVM.sendMessage(conversationId!!, senderId, msg)
+
+                chatTextInp.editText?.setText("")
+            }
+        }
+
+        if (targetPhotoUrl != null && targetPhotoUrl.length > 5) {
+            Picasso.get().load(targetPhotoUrl)
+                .into(chatProfileImgView)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        if (conversationId != null) {
+            userVM.observeConversation(conversationId!!).observe(this, Observer {
+                chatTextView.text = it["messages"].toString()
+            })
+        }
     }
 }
