c0f475407fc74e1bd0ec2838d9c7338a841f355c
Amol <amolborkar5@gmail.com>
2020-03-31 19:58:18 +0530

    New activity for showing complete user details

---
+++ app/src/main/java/com/halalrishtey/UserDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.halalrishtey
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UserDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_detail)
+    }
+}
