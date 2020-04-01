e22437e4a5f22222739c2bdb0b47f6b1afd13130
Amol <amolborkar5@gmail.com>
2020-04-01 23:53:53 +0530

    Added back button

--- app/src/main/java/com/halalrishtey/UserDetailActivity.kt
+++ app/src/main/java/com/halalrishtey/UserDetailActivity.kt
@@ -2,11 +2,16 @@ package com.halalrishtey
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_user_detail.*
 
 class UserDetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_detail)
+        setSupportActionBar(userDetailToolbar)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
     }
 }
