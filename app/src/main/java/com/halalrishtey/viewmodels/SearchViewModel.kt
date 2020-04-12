7ee1bf695abbf97c911effd1273f700df4ddb2b9
Amol <amolborkar5@gmail.com>
2020-04-12 22:23:12 +0530

    Improved apply filters function

--- app/src/main/java/com/halalrishtey/viewmodels/SearchViewModel.kt
+++ app/src/main/java/com/halalrishtey/viewmodels/SearchViewModel.kt
@@ -3,7 +3,7 @@ package com.halalrishtey.viewmodels
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.halalrishtey.models.User
+import com.halalrishtey.models.ProfileCardData
 
 data class SearchParams(
     var minAge: Int = 0,
@@ -23,27 +23,46 @@ class SearchViewModel : ViewModel() {
     val filters = MutableLiveData<SearchParams>(null)
     private val defaultValues = SearchParams()
 
-    fun applySearchFilters(listOfUsers: ArrayList<User>): ArrayList<User> {
+    fun applySearchFilters(listOfUsers: ArrayList<ProfileCardData>): ArrayList<ProfileCardData> {
         var r = listOfUsers
 
-        if (query.value != null && query.value!!.length > 2)
-            r = listOfUsers.filter { u -> u.displayName.contains(query.value!!) } as ArrayList<User>
+        if (!query.value.isNullOrBlank())
+            r =
+                r.filter { u ->
+                    u.data.displayName.contains(
+                        query.value!!,
+                        ignoreCase = true
+                    )
+                } as ArrayList<ProfileCardData>
 
         filters.value?.let {
             if (it.minAge != defaultValues.minAge)
-                r = r.filter { u -> u.age!! + 18 > it.minAge } as ArrayList<User>
+                r = r.filter { u -> u.data.age!! > it.minAge + 18 } as ArrayList<ProfileCardData>
 
             if (it.maxAge != defaultValues.maxAge)
-                r = r.filter { u -> u.age!! + 18 < it.maxAge } as ArrayList<User>
+                r = r.filter { u -> u.data.age!! < it.maxAge + 18 } as ArrayList<ProfileCardData>
 
             //Convert both values to cm before comparing
             if (it.minHeight != it.minHeight)
                 r =
-                    r.filter { u -> u.height[0].toInt() * 30.5 > it.minHeight + 137 } as ArrayList<User>
+                    r.filter { u -> u.data.height[0].toInt() * 30.5 > it.minHeight + 137 } as ArrayList<ProfileCardData>
 
             if (it.maxHeight != it.maxHeight)
                 r =
-                    r.filter { u -> u.height[0].toInt() * 30.5 < it.maxHeight + 137 } as ArrayList<User>
+                    r.filter { u -> u.data.height[0].toInt() * 30.5 < it.maxHeight + 137 } as ArrayList<ProfileCardData>
+
+            if (it.location.isNotBlank())
+                r =
+                    r.filter { u ->
+                        u.data.address.contains(
+                            it.location,
+                            ignoreCase = true
+                        )
+                    } as ArrayList<ProfileCardData>
+
+            if (it.highestEdu != "Qualification")
+                r =
+                    r.filter { u -> u.data.education == it.highestEdu } as ArrayList<ProfileCardData>
         }
 
         Log.d("SearchViewModel", "Applying filters removed ${listOfUsers.size - r.size} profiles!")
