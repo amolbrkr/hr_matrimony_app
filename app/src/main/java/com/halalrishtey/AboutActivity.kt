cd91c0ab97b0058d7b9e59bc873f14a9561249d6
Amol <amolborkar5@gmail.com>
2020-04-15 19:29:40 +0530

    Added initial content for about page

--- app/src/main/java/com/halalrishtey/AboutActivity.kt
+++ app/src/main/java/com/halalrishtey/AboutActivity.kt
@@ -1,12 +1,21 @@
 package com.halalrishtey
 
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import kotlinx.android.synthetic.main.activity_about.*
 
 class AboutActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+        setSupportActionBar(aboutToolbar as Toolbar)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+
+        aboutText.movementMethod = LinkMovementMethod.getInstance()
     }
 }
