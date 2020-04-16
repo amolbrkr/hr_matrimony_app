f2f3e26e6d1722d52881101128d633c4f3fa199b
Amol <amolborkar5@gmail.com>
2020-04-16 14:26:04 +0530

    Added functionality for all the options except message

--- app/src/main/java/com/halalrishtey/ProfileOptionsSheet.kt
+++ app/src/main/java/com/halalrishtey/ProfileOptionsSheet.kt
@@ -2,8 +2,12 @@ package com.halalrishtey
 
 import android.app.Dialog
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.halalrishtey.models.User
@@ -12,6 +16,35 @@ import kotlinx.android.synthetic.main.profile_options_sheet.view.*
 
 class ProfileOptionsSheet(val user: User) : BottomSheetDialogFragment() {
     private val userVM: UserViewModel by activityViewModels()
+
+    private fun genInterestBtnListener(
+        isShortlisted: Boolean,
+        targetUserId: String,
+        v: View
+    ): View.OnClickListener {
+        return View.OnClickListener {
+            if (!isShortlisted) {
+                v.showIntIcon.setImageResource(R.drawable.ic_favorite)
+                userVM.initInterest(
+                    userVM.currentUid.value!!,
+                    userVM.currentUser.value?.displayName!!,
+                    targetUserId
+                ).observe(viewLifecycleOwner, Observer { msg ->
+                    Toast.makeText(context, msg, Toast.LENGTH_SHORT)
+                        .show()
+                })
+            } else {
+                v.showIntIcon.setImageResource(R.drawable.ic_favorite_border)
+                userVM.removeInterest(
+                    userVM.currentUid.value!!,
+                    targetUserId
+                ).observe(viewLifecycleOwner, Observer { msg ->
+                    Toast.makeText(context, msg, Toast.LENGTH_SHORT)
+                        .show()
+                })
+            }
+        }
+    }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val v = layoutInflater.inflate(R.layout.profile_options_sheet, null)
@@ -22,12 +55,29 @@ class ProfileOptionsSheet(val user: User) : BottomSheetDialogFragment() {
                 action = Intent.ACTION_SEND
                 putExtra(
                     Intent.EXTRA_TEXT,
-                    "Check out ${user.displayName}'s profile on Halal Rishtey App!\n\n Get the app now from #"
+                    "Name: ${user.displayName}\n" +
+                            "Age: ${user.age}\n" +
+                            "Height: ${user.height}\n" +
+                            "Income: ${user.annualIncome} PA\n" +
+                            "Check out ${user.displayName}'s profile at Halal Rishtey app! \n\n Get the app now from #"
                 )
                 type = "text/plain"
             }
             startActivity(i)
         }
+
+        v.dirContactOpt.setOnClickListener {
+            val intent = Intent(Intent.ACTION_DIAL)
+            intent.data = Uri.parse("tel:${user.phoneNumber}")
+            startActivity(intent)
+        }
+
+        val isShortlisted =
+            userVM.currentUser.value?.interestedProfiles?.contains(
+                user.uid
+            ) ?: false
+
+        v.showInterestOpt.setOnClickListener(genInterestBtnListener(isShortlisted, user.uid!!, v))
 
         val d = BottomSheetDialog(requireContext())
         d.setContentView(v)
