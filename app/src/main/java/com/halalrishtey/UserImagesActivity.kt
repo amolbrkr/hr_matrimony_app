5287377c046310e0feda588e99088d80f8a7f22e
Amol <amolborkar5@gmail.com>
2020-04-20 19:46:01 +0530

    New dedicated screen for showing images with zoom and pan gestures

---
+++ app/src/main/java/com/halalrishtey/UserImagesActivity.kt
@@ -0,0 +1,34 @@
+package com.halalrishtey
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.halalrishtey.adapter.DefaultImageAdapter
+import kotlinx.android.synthetic.main.activity_user_images.*
+
+class UserImagesActivity() : AppCompatActivity() {
+    private lateinit var adapter: DefaultImageAdapter
+    private var pos = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_images)
+
+        val images = intent.getStringArrayListExtra("images")!!
+        pos = intent.getIntExtra("pos", 0)
+
+        adapter = DefaultImageAdapter(images)
+        userImagesRV.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        userImagesRV.adapter = adapter
+
+        backBtn.setOnClickListener {
+            this.finish()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        userImagesRV.layoutManager?.scrollToPosition(pos)
+    }
+}
