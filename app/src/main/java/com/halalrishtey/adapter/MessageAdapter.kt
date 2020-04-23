4cb6f53f67a6ff22eefa6d58fbfb101835265b8d
Amol <amolborkar5@gmail.com>
2020-04-23 20:53:02 +0530

    Added logic for showing meetup prompt

--- app/src/main/java/com/halalrishtey/adapter/MessageAdapter.kt
+++ app/src/main/java/com/halalrishtey/adapter/MessageAdapter.kt
@@ -20,6 +20,7 @@ class MessageAdapter(private val senderId: String, private val data: List<Messag
 
         val inflatedView = if (viewType == 0)
             inflater.inflate(R.layout.in_message, parent, false)
+        else if (viewType == 2) inflater.inflate(R.layout.meetup_prompt, parent, false)
         else inflater.inflate(R.layout.out_message, parent, false)
 
         return MessageVH(inflatedView)
@@ -38,6 +39,9 @@ class MessageAdapter(private val senderId: String, private val data: List<Messag
     }
 
     override fun getItemViewType(position: Int): Int {
+        //On every multiple of a value show meetup prompt
+        if (position % 10 == 0) return 2
+
         return if (data[position].senderId == senderId) 1 else 0
     }
 
