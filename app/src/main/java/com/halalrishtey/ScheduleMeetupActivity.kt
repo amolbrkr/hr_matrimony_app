dd05e9c982b66d0b8aa1e64d8d5fdea59e973e3b
Amol <amolborkar5@gmail.com>
2020-04-26 01:02:32 +0530

    New activity for scheduling meetups

---
+++ app/src/main/java/com/halalrishtey/ScheduleMeetupActivity.kt
@@ -0,0 +1,12 @@
+package com.halalrishtey
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ScheduleMeetupActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_schedule_meetup)
+    }
+}
