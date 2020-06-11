f6fce2f980d57e900bb2f3c5a2c9da34ea8f6ab8
Amol <amolborkar5@gmail.com>
2020-06-11 17:33:12 +0530

    Fixed some bugs

--- app/src/main/java/com/halalrishtey/ProfileOptionsSheet.kt
+++ app/src/main/java/com/halalrishtey/ProfileOptionsSheet.kt
@@ -4,13 +4,14 @@ import android.app.Dialog
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.snackbar.Snackbar
 import com.halalrishtey.models.User
 import com.halalrishtey.viewmodels.UserViewModel
 import kotlinx.android.synthetic.main.profile_options_sheet.view.*
@@ -18,57 +19,22 @@ import kotlinx.android.synthetic.main.profile_options_sheet.view.*
 class ProfileOptionsSheet(val user: User) : BottomSheetDialogFragment() {
     private val userVM: UserViewModel by activityViewModels()
 
-    private fun genInterestBtnListener(
-        isShortlisted: Boolean,
-        targetUserId: String,
-        v: View
-    ): View.OnClickListener {
-        return View.OnClickListener {
-            if (!isShortlisted) {
-                v.showIntIcon.setImageResource(R.drawable.ic_favorite)
-                userVM.initInterest(
-                    userVM.currentUid.value!!,
-                    userVM.currentUser.value?.displayName!!,
-                    targetUserId
-                ).observe(viewLifecycleOwner, Observer { msg ->
-                    Toast.makeText(context, msg, Toast.LENGTH_SHORT)
-                        .show()
-                })
-            } else {
-                v.showIntIcon.setImageResource(R.drawable.ic_favorite_border)
-                userVM.removeInterest(
-                    userVM.currentUid.value!!,
-                    targetUserId
-                ).observe(viewLifecycleOwner, Observer { msg ->
-                    Toast.makeText(context, msg, Toast.LENGTH_SHORT)
-                        .show()
-                })
-            }
-        }
-    }
-
-    private fun genMessageBtnListener(
-        currentUser: User,
-        targetUser: User
-    ): View.OnClickListener {
-        return View.OnClickListener {
-            userVM.initConversation(currentUser, targetUser)
-                .observe(viewLifecycleOwner, Observer {
-                    if (it.length > 1) {
-                        val i = Intent(context, ChatActivity::class.java)
-                        i.putExtra("conversationId", it)
-                        i.putExtra("currentId", currentUser.uid)
-                        i.putExtra("targetId", targetUser.uid)
-                        i.putExtra("targetPhotoUrl", targetUser.photoUrl)
-                        i.putExtra("targetName", targetUser.displayName)
-                        startActivity(i)
-                    }
-                })
-        }
-    }
-
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val v = LayoutInflater.from(requireContext()).inflate(R.layout.profile_options_sheet, null)
+
+        v.meetupOpt.setOnClickListener {
+            val current = userVM.currentUser.value?.uid
+            val target = user.uid
+            if (current != null && target != null) {
+                val i = Intent(context, ScheduleMeetupActivity::class.java)
+                i.apply {
+                    putExtra("currentId", current)
+                    putExtra("targetId", target)
+                }
+                startActivity(i)
+            } else Snackbar.make(requireView(), "Something went wrong!", Snackbar.LENGTH_SHORT)
+                .show()
+        }
 
         v.shareProfileOpt.setOnClickListener {
 
@@ -93,15 +59,49 @@ class ProfileOptionsSheet(val user: User) : BottomSheetDialogFragment() {
             startActivity(intent)
         }
 
+        Log.d("ProfileOptions", "Current user: ${userVM.currentUser.value}")
         val isShortlisted =
             userVM.currentUser.value?.interestedProfiles?.contains(
                 user.uid
             ) ?: false
 
-        v.showInterestOpt.setOnClickListener(genInterestBtnListener(isShortlisted, user.uid!!, v))
-
-        v.messageOpt.setOnClickListener(genMessageBtnListener(userVM.currentUser.value!!, user))
-
+        v.showInterestOpt.setOnClickListener {
+            if (!isShortlisted) {
+                v.showIntIcon.setImageResource(R.drawable.ic_favorite)
+                userVM.initInterest(
+                    userVM.currentUid.value!!,
+                    userVM.currentUser.value?.displayName!!,
+                    user.uid!!
+                ).observe(viewLifecycleOwner, Observer { msg ->
+                    Toast.makeText(context, msg, Toast.LENGTH_SHORT)
+                        .show()
+                })
+            } else {
+                v.showIntIcon.setImageResource(R.drawable.ic_favorite_border)
+                userVM.removeInterest(
+                    userVM.currentUid.value!!,
+                    user.uid!!
+                ).observe(viewLifecycleOwner, Observer { msg ->
+                    Toast.makeText(context, msg, Toast.LENGTH_SHORT)
+                        .show()
+                })
+            }
+        }
+        v.messageOpt.setOnClickListener {
+            val currentUser = userVM.currentUser.value!!
+            userVM.initConversation(userVM.currentUser.value!!, user)
+                .observe(viewLifecycleOwner, Observer {
+                    if (it.length > 1) {
+                        val i = Intent(context, ChatActivity::class.java)
+                        i.putExtra("conversationId", it)
+                        i.putExtra("currentId", currentUser.uid)
+                        i.putExtra("targetId", user.uid)
+                        i.putExtra("targetPhotoUrl", user.photoUrl)
+                        i.putExtra("targetName", user.displayName)
+                        startActivity(i)
+                    }
+                })
+        }
         val d = BottomSheetDialog(requireContext())
         d.setContentView(v)
         return d
