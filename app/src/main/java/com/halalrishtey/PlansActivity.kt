5ad77d9d0dd686e1091d09c7f473adace2012ab1
Amol <amolborkar5@gmail.com>
2020-06-16 23:28:23 +0530

    Added & configured payment gateway (working)

--- app/src/main/java/com/halalrishtey/PlansActivity.kt
+++ app/src/main/java/com/halalrishtey/PlansActivity.kt
@@ -5,20 +5,27 @@ import android.text.SpannableString
 import android.text.Spanned
 import android.text.style.ForegroundColorSpan
 import android.text.style.RelativeSizeSpan
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.snackbar.Snackbar
 import com.halalrishtey.adapter.PlansAdapter
 import com.halalrishtey.models.PlanItem
 import com.halalrishtey.viewmodels.UserViewModel
+import com.razorpay.Checkout
+import com.razorpay.PaymentResultListener
 import kotlinx.android.synthetic.main.activity_plans.*
+import org.json.JSONObject
 
-class PlansActivity : AppCompatActivity() {
+
+class PlansActivity : AppCompatActivity(), PaymentResultListener {
     private val userVM: UserViewModel by viewModels()
     private lateinit var adapter: PlansAdapter
     private lateinit var plans: ArrayList<PlanItem>
+    private lateinit var checkout: Checkout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,9 +36,14 @@ class PlansActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         plans = ArrayList()
-        adapter = PlansAdapter(plans)
+        adapter = PlansAdapter(plans, userVM.currentUid.value!!) { opts -> startPayment(opts) }
         plansRV.layoutManager = LinearLayoutManager(this);
         plansRV.adapter = adapter
+
+        //Payment Gateway
+        checkout = Checkout()
+        checkout.setKeyID("rzp_test_dvq5JlDApXAZWu")
+        Checkout.preload(applicationContext)
     }
 
     override fun onStart() {
@@ -80,5 +92,34 @@ class PlansActivity : AppCompatActivity() {
                 Toast.makeText(this, "No Plans to Show!", Toast.LENGTH_SHORT).show()
             }
         })
+    }
+
+    //p0: Error code, p1: Error string
+    override fun onPaymentError(p0: Int, p1: String?) {
+        val snack = Snackbar.make(
+            window.decorView.findViewById(android.R.id.content),
+            "Payment Error: $p0, $p1", Snackbar.LENGTH_INDEFINITE
+        )
+        snack.setAction("OK") { snack.dismiss() }
+    }
+
+    //p0: Payment Id
+    override fun onPaymentSuccess(p0: String?) {
+        val snack = Snackbar.make(
+            window.decorView.findViewById(android.R.id.content),
+            "Thank you for choosing us! Your plan will be updated in a minute",
+            Snackbar.LENGTH_INDEFINITE
+        )
+        snack.setAction("OK") { snack.dismiss() }
+    }
+
+    fun startPayment(options: JSONObject) {
+//        checkout.setImage(R.drawable.ic_star) //For Logo
+        try {
+            checkout.open(this, options)
+        } catch (e: Exception) {
+            Toast.makeText(this, "Payment Error: ${e.message}", Toast.LENGTH_LONG).show()
+            Log.e("PlansActivity", "Error in starting Razorpay Checkout", e)
+        }
     }
 }
--- app/src/main/java/com/halalrishtey/adapter/PlansAdapter.kt
+++ app/src/main/java/com/halalrishtey/adapter/PlansAdapter.kt
@@ -4,14 +4,16 @@ import android.graphics.Paint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.halalrishtey.R
 import com.halalrishtey.models.PlanItem
 import kotlinx.android.synthetic.main.plan_item.view.*
+import org.json.JSONObject
 
 class PlansAdapter(
-    private val planList: ArrayList<PlanItem>
+    private val planList: ArrayList<PlanItem>,
+    private val currentUid: String,
+    private val initPaymentCb: (JSONObject) -> Unit
 ) : RecyclerView.Adapter<PlansAdapter.PlansVH>() {
 
     class PlansVH(v: View) : RecyclerView.ViewHolder(v)
@@ -32,7 +34,19 @@ class PlansAdapter(
                 "₹ ${planList[position].discountPrice} for ${planList[position].validity} month"
 
             pBuyBtn.setOnClickListener {
-                Toast.makeText(this.context, "Coming Soon!", Toast.LENGTH_SHORT).show();
+                val options = JSONObject()
+                options.put("name", "Nikahal Services Pvt Ltd")
+                options.put(
+                    "description",
+                    "Reference No: ${System.currentTimeMillis()}_${currentUid}_${planList[position].id}"
+                )
+                options.put("image", "https://s3.amazonaws.com/rzp-mobile/images/rzp.png")
+//                options.put("order_id", "order_9A33XWu170gUtm")
+                options.put("currency", "INR")
+
+                //Amount is always passed in currency subunits (PAISE)  * Eg: "500" = INR 5.00
+                options.put("amount", "${planList[position].discountPrice * 100}")
+                initPaymentCb(options)
             }
         }
     }
